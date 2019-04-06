6dd42fbb08a23d6cb341b973531d6b576ca499a1
ahmedtarek97 <36510613+ahmedtarek97@users.noreply.github.com>
2019-04-06 22:49:21 +0200

    Add files via upload

---
+++ Process.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schdular
+{
+    class Process
+    {
+
+       private int priority;
+        private int brust_time;
+        private int arrival_time;
+
+        public int Priority { get; set; }
+        public int Brust_time { get; set; }
+        public int Arrival_time { get; set; }
+
+        public Process()
+        {
+
+
+            priority = 0;
+
+            brust_time = 0;
+
+            arrival_time = 0;
+
+
+        }
+
+
+
+    }
+}
